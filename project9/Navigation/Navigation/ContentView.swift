08a843e8e404338e4d5e64fbef42bd174b12e6f7
caiqfrrz <caiqfrrz@gmail.com>
2024-08-09 14:49:22 -0300

    done project 9

--- project9/Navigation/Navigation/ContentView.swift
+++ project9/Navigation/Navigation/ContentView.swift
@@ -7,21 +7,57 @@
 
 import SwiftUI
 
-struct Student {
-    var id = UUID()
-    var name: String
-    var age: Int
+class MyPath {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try? JSONEncoder().encode(representation)
+            try data?.write(to: savePath)
+        } catch {
+            print("Failed to save navigation path.")
+        }
+    }
+    
+}
+
+struct DetailView: View {
+    let number: Int
+    
+    var body: some View {
+        NavigationLink("Go to new view", value: Int.random(in: 1...100))
+            .navigationTitle("Number: \(number)")
+    }
 }
 
 struct ContentView: View {
+    @State private var myPath = MyPath()
+    
     var body: some View {
-        NavigationStack {
-            List(0..<100) { number in
-                NavigationLink("Row \(number)", value: number)
-            }
-            .navigationDestination(for: Int.self) { selected in
-                Text("Hello \(selected)")
-            }
+        NavigationStack(path: $myPath.path) {
+            DetailView(number: 0)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i)
+                }
         }
     }
 }

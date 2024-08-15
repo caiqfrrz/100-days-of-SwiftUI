5522751bd6cff0537fcb6ed8340c6956989b21d7
caiqfrrz <caiqfrrz@gmail.com>
2024-08-15 19:01:07 -0300

    created project 10

---
+++ project10/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Caique Ferraz on 15/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

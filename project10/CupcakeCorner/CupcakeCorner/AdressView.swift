b36e1d7723e6a7d842a85df63a80fe3a96645c6d
caiqfrrz <caiqfrrz@gmail.com>
2024-08-19 20:54:28 -0300

    Project 10 done
    Done project 10 tutorials, still have challenge to complete tho

---
+++ project10/CupcakeCorner/CupcakeCorner/AdressView.swift
@@ -0,0 +1,18 @@
+//
+//  AdressView.swift
+//  CupcakeCorner
+//
+//  Created by Caique Ferraz on 16/08/24.
+//
+
+import SwiftUI
+
+struct AdressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdressView()
+}
---
+++ project10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,74 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Caique Ferraz on 19/08/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("Your total cost is \(order.cost, format: .currency(code: "BRL"))")
+                    .font(.title)
+                
+                Button("Place order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                .padding()
+            }
+        }
+        .navigationTitle("Checkout")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order of \(decodedOrder.quantity) \(Order.types[decodedOrder.type]) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Check out failed: \(error.localizedDescription)")
+        }
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
